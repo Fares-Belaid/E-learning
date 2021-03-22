67f93b0e4121e5964173ed1b55ec103378c68638
Fares-Belaid <belaid.fares1995@gmail.com>
2021-03-22 12:52:58 +0100

    first commit

---
+++ src/test/java/com/pfa/elearning/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.elearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
