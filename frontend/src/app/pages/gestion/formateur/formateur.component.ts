1bcb42a664bed9433566addb3e5c875c8e9b4cd4
Fares-Belaid <belaid.fares1995@gmail.com>
2021-06-13 14:14:33 +0100

    Admin dashboard

--- frontend/src/app/pages/gestion/formateur/formateur.component.ts
+++ frontend/src/app/pages/gestion/formateur/formateur.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Formateur } from 'src/app/models/formateur';
 import { FormateurService } from 'src/app/services/formateur.service';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-formateur',
   templateUrl: './formateur.component.html',
@@ -34,14 +34,45 @@ export class FormateurComponent implements OnInit {
   createFormateur(){
     this.router.navigate(['create-formateur']);
   }
+  /*
   deleteFormateur(id: number){
     this.formateurService.deleteFormateur(id).subscribe(data => {
       console.log(data);
       this.getAllFormateurs();
     })
+  }*/
+
+  deleteFormateur(id: number){
+    Swal.fire({
+      title: 'Are you sure want to remove?',
+      text: 'You will not be able to recover this file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.value) {
+        this.formateurService.deleteFormateur(id).subscribe(data => {
+          console.log(data);
+          this.getAllFormateurs();
+        })
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+        )
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
   }
 
-
+  
+   
   formateurDetails(id: number){
     this.router.navigate(['details-formateur',id]);
   }
